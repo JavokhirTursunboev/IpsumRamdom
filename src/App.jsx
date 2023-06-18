5560e04794cad2e4ccc55a432f4b7c56f29a6454
Javokhir <jtursunboev@bk.ru>
2023-06-18 21:26:55 +0900

    app js is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
+import { useState } from "react";
+import data from "./data";
 function App() {
-  return <>app</>;
+  const [count, setCount] = useState(1);
+  return (
+    <section className="section-center">
+      <h4>tired of boring lorem ipusm?</h4>
+      <form className="lorem-from">
+        <label htmlFor="amount">paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          min="1"
+          max="8"
+          step="1"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button className="btn">Generate</button>
+      </form>
+    </section>
+  );
 }
 
 export default App;
